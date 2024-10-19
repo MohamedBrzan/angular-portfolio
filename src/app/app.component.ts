327493ebac3d4860f80795d8b8945b92f84dc929
MohamedBrzan <mohamedbrzan11@gmail.com>
2024-10-19 17:20:19 +0300

    stable version ( finished projects page )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,21 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { CustomCursorComponent } from './custom-cursor/custom-cursor.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ProjectsComponent, CustomCursorComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ProjectsComponent,
+    CustomCursorComponent,
+    FontAwesomeModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor() {
-  }
+  constructor() {}
 }
